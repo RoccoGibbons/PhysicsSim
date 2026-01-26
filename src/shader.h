3dacf9d8227417f25c2dc2295b7465f86442e2ac
rocco gibbons <roccogibbons@outlook.com>
2026-01-26 11:04:37 +0000

    Tweaked with fragment shader colour outputs and began to move shader code into a separate file: still need to create getter functions for uniforms and put the actual vertex and fragment shader code into their own files

---
+++ src/shader.h
@@ -0,0 +1,83 @@
+#include <glad/glad.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    unsigned int ID;
+} Shader;
+
+Shader initialise(const char* vertexPath, const char* fragmentPath);
+int length(char* myString);
+void checkCompileErrors(unsigned int shader, char* type);
+void use(Shader myShader);
+// Need to create uniform setter functions.
+
+Shader initialise(const char* vertexPath, const char* fragmentPath) {
+    Shader myShader;
+    
+    FILE* vShaderFile = fopen(vertexPath, "r");
+    if (vShaderFile == NULL) {
+        printf("ERROR::SHADER::VERTEX::FILE_NOT_READ");
+    }
+
+    FILE* fShaderFile = fopen(fragmentPath, "r");
+    if (fShaderFile == NULL) {
+        printf("ERROR::SHADER::FRAGMENT::FILE_NOT_READ");
+    }
+
+    char buffer[256];
+    char* vertexCode;
+    while(fgets(buffer, 256, vShaderFile)) {
+        strncat(vertexCode, buffer, length(buffer));
+    }
+
+    char buffer[256];
+    char* fragmentCode;
+    while(fgets(buffer, 256, fShaderFile)) {
+        strncat(fragmentCode, buffer, length(buffer));
+    }
+
+    unsigned int vertex, fragment;
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vertexCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fragmentCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+
+    myShader.ID = glCreateProgram();
+    glAttachShader(myShader.ID, vertex);
+    glAttachShader(myShader.ID, fragment);
+    glLinkProgram(myShader.ID);
+    checkCompileErrors(myShader.ID, "PROGRAM");
+
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+
+    return myShader;
+}
+
+int length(char* myString) {
+    int counter = 0;
+    while (myString[counter] != '\n') {
+        counter += 1;
+    }
+    return counter;
+}
+
+void checkCompileErrors(unsigned int shader, char* type) {
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        printf("ERROR::SHADER::%s::COMPILATION_FAILED %s /n", type, infoLog);
+    }
+}
+
+void use(Shader myShader) {
+    glUseProgram(myShader.ID);
+}
