805c63c2bfc87ddb4ade278f31f2e754912cdab7
Rocco Gibbons <roccogibbons@outlook.com>
2026-02-01 12:22:04 +0000

    moved into separate files, currently broken

--- build/external/glfw/cmake_uninstall.cmake
+++ build/external/glfw/cmake_uninstall.cmake
@@ -1,22 +1,22 @@
 
-if (NOT EXISTS "C:/Users/rocco/code/physics_sim/build/external/glfw/install_manifest.txt")
-    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/rocco/code/physics_sim/build/external/glfw/install_manifest.txt\"")
+if (NOT EXISTS "C:/Users/rocco/code/Physics sim/build/external/glfw/install_manifest.txt")
+    message(FATAL_ERROR "Cannot find install manifest: \"C:/Users/rocco/code/Physics sim/build/external/glfw/install_manifest.txt\"")
 endif()
 
-file(READ "C:/Users/rocco/code/physics_sim/build/external/glfw/install_manifest.txt" files)
+file(READ "C:/Users/rocco/code/Physics sim/build/external/glfw/install_manifest.txt" files)
 string(REGEX REPLACE "\n" ";" files "${files}")
 
 foreach (file ${files})
   message(STATUS "Uninstalling \"$ENV{DESTDIR}${file}\"")
   if (EXISTS "$ENV{DESTDIR}${file}")
-    exec_program("C:/mingw64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    exec_program("C:/Program Files/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
       MESSAGE(FATAL_ERROR "Problem when removing \"$ENV{DESTDIR}${file}\"")
     endif()
   elseif (IS_SYMLINK "$ENV{DESTDIR}${file}")
-    EXEC_PROGRAM("C:/mingw64/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
+    EXEC_PROGRAM("C:/Program Files/CMake/bin/cmake.exe" ARGS "-E remove \"$ENV{DESTDIR}${file}\""
                  OUTPUT_VARIABLE rm_out
                  RETURN_VALUE rm_retval)
     if (NOT "${rm_retval}" STREQUAL 0)
