98b10f64830666ee6fcd13ef1cdb64dbe2456953
rocco gibbons <roccogibbons@outlook.com>
2026-03-10 20:04:44 +0000

    git updating

--- src/simulation_calculations.h
+++ src/simulation_calculations.h
@@ -12,7 +12,7 @@ typedef struct Object {
     char* type;
     vec3 position;
     float radius;
-    float velocity;
+    float speed;
     float bearing;
 } Object;
 
@@ -37,18 +37,41 @@ void calcCollision(Object *obj1, Object *obj2, float e) {
 void boundaryCollision(Object *obj, Object *wall, float e) {
     vec3 bearingVector = {sin(obj->bearing), cos(obj->bearing), 0};
     vec3 wallVector = GLM_VEC3_ZERO_INIT;
+    char* wallDirection; 
+
     if (wall->bearing == 0.0f){
         glm_vec3_add(wallVector, (vec3){1, 0, 0}, wallVector);
+        wallDirection = "HORIZONTAL";
     } else if (wall->bearing == glm_rad(90.0f)) {
         glm_vec3_add(wallVector, (vec3){0, 1, 0}, wallVector);
+        wallDirection = "VERTICAL";
     } else {
         printf("ERROR::BOUNDARY_CALC::WALL_VECTOR\n");
     }
     
     float angleOfApproach = acos( (abs(glm_vec3_dot(bearingVector, wallVector))) / (glm_vec3_norm(bearingVector) * glm_vec3_norm(wallVector)));
-    printf("%f", angleOfApproach);
     
+    float angleOfDeflection = atan(e * tan(angleOfApproach));
+
+    float speed = sqrt( pow((e * obj->speed * sin(angleOfApproach)), 2) + pow((obj->speed * cos(angleOfApproach)), 2) );
+
+    float newBearing;
+
     
+    // BACKUP IF NO OTHER SOL.
+
+    // if (strcmp(wallDirection, "HORIZONTAL") == 0 && (obj->bearing >= 0 && obj->bearing < glm_rad(180.0f))) {
+    //     printf("");
+    // } else if (strcmp(wallDirection, "HORIZONTAL") == 0 && (obj->bearing >= glm_rad(180.0f) && obj->bearing < glm_rad(360.0f))) {
+    //     printf("");
+    // } else if (strcmp(wallDirection, "VERTICAL") == 0 && (obj->bearing >= 0 && obj->bearing < glm_rad(180.0f))) {
+    //     printf("");
+    // } else if (strcmp(wallDirection, "VERTICAL") == 0 && (obj->bearing >= glm_rad(180.0f) && obj->bearing < glm_rad(360.0f))) {
+    //     printf("");
+    // } else {
+    //     printf("ERROR::BOUNDARY_CALC::NEW_BEARING\n");
+    // }
+
      
 }
 
