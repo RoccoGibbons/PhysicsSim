800c2ca6351ac84ec02c47f9fc9ffeb6747202db
Rocco Gibbons <roccogibbons@outlook.com>
2026-04-07 19:55:41 +0100

    Included nuklear glfw3 implementation

--- src/main.c
+++ src/main.c
@@ -7,13 +7,25 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <cglm/cglm.h>
+
+// Nuklear GUI library
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
 #define NK_IMPLEMENTATION
+#define NK_GLFW_GL4_IMPLEMENTATION
 #include <nuklear/nuklear.h>
+#include <nuklear/nuklear_glfw_gl3.h>
 
 // Standard C libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <math.h>
 #include <string.h>
 
--- src/user_interface.h
+++ src/user_interface.h
@@ -1,8 +1,8 @@
 #include <glad/glad.h>
 #include <cglm/cglm.h>
 #include <stdio.h>
-#define NK_IMPLEMENTATION
-#include <nuklear/nuklear.h>
+// #define NK_IMPLEMENTATION
+// #include <nuklear/nuklear.h>
 
 
 void toggleNavbar() {
